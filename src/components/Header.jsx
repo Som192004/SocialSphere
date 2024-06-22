7b3430e7b465479926f096ef49c359a1393fef2d
Soham <sohamholkar800@gmail.com>
2024-06-22 22:31:25 +0530

    Now Deployed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
         </a>
 
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><Link to = "/" className="nav-link px-2 text-white">Home</Link></li>
-          <li><Link to="/features" className="nav-link px-2 text-white">Features</Link></li>
-          <li><Link to = "/about" className="nav-link px-2 text-white">About</Link></li>
+          <li><Link to = "/SocialSphere/Home" className="nav-link px-2 text-white">Home</Link></li>
+          <li><Link to="/SocialSphere/features" className="nav-link px-2 text-white">Features</Link></li>
+          <li><Link to = "/SocialSphere/about" className="nav-link px-2 text-white">About</Link></li>
         </ul>
 
         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
